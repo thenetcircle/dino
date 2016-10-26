c404f0a43db25715d43fbbcfb0e011153229b51b
Oscar Eriksson <oscar.eriks@gmail.com>
2016-10-26 15:00:24 +0800

    delete acls for channels

--- dino/admin/static/custom/js/users_in_room.js
+++ dino/admin/static/custom/js/users_in_room.js
@@ -20,27 +20,4 @@ $(document).ready(function() {
             img.addClass('fa-caret-square-o-right');
         }
     });
-
-    $('a.acl-remove').confirm({
-        text: "Are you sure you want to delete the permission?",
-        title: "Confirmation required",
-        confirm: function(button) {
-            remove_url = $(button).find('input.acl-url').val()
-            $.ajax({
-                method: 'DELETE',
-                url: remove_url
-            }).done(function(data) {
-                $(button).closest('tr').remove();
-            });
-        },
-        cancel: function(button) {
-            // nothing to do
-        },
-        confirmButton: "Yes",
-        cancelButton: "No",
-        post: true,
-        confirmButtonClass: "btn-danger",
-        cancelButtonClass: "btn-default",
-        dialogClass: "modal-dialog modal-lg" // Bootstrap classes for large modal
-    });
 });
