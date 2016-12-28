e4fa41214d03d5c0163fe3cc4d3479d3b5007ccb
Oscar Eriksson <oscar.eriks@gmail.com>
2016-12-28 15:06:24 +0800

    some safeguards against blank user ids when disconnecting

--- dino/hooks/disconnect.py
+++ dino/hooks/disconnect.py
@@ -25,32 +25,44 @@ __author__ = 'Oscar Eriksson <oscar.eriks@gmail.com>'
 class OnDisconnectHooks(object):
     @staticmethod
     def leave_private_room(arg: tuple):
-        # todo: only broadcast 'offline' status if current status is 'online' (i.e. don't broadcast if e.g. 'invisible')
-        user_id = str(environ.env.session.get(SessionKeys.user_id.value))
-        user_name = environ.env.session.get(SessionKeys.user_name.value)
-        logger.debug('a user disconnected, id "%s" name "%s"' % (user_id, user_name))
+        data, activity = arg
+        try:
+            # todo: only broadcast 'offline' status if current status is 'online' (i.e. don't broadcast if e.g. 'invisible')
+            user_id = activity.actor.id
+            user_name = environ.env.session.get(SessionKeys.user_name.value)
+            logger.debug('a user disconnected, id "%s" name "%s"' % (user_id, user_name))
 
-        if user_id is None or len(user_id.strip()) == 0:
-            return
-        environ.env.leave_room(user_id)
+            if user_id is None or len(user_id.strip()) == 0:
+                return
+            environ.env.leave_room(user_id)
+        except Exception as e:
+            logger.error('could not leave private room: %s' % str(e))
+            logger.debug('request for failed leave_private_room(): %s' % str(data))
+            logger.exception(traceback.format_exc())
 
     @staticmethod
     def leave_all_public_rooms_and_emit_leave_events(arg: tuple):
-        user_id = str(environ.env.session.get(SessionKeys.user_id.value))
-        user_name = environ.env.session.get(SessionKeys.user_name.value)
-        rooms = environ.env.db.rooms_for_user(user_id)
+        data, activity = arg
+        try:
+            user_id = activity.actor.id
+            user_name = environ.env.session.get(SessionKeys.user_name.value)
+            rooms = environ.env.db.rooms_for_user(user_id)
 
-        for room_id, room_name in rooms.items():
-            utils.remove_user_from_room(user_id, user_name, room_id)
-            environ.env.emit('gn_user_left', utils.activity_for_leave(user_id, user_name, room_id, room_name), room=room_id)
+            for room_id, room_name in rooms.items():
+                utils.remove_user_from_room(user_id, user_name, room_id)
+                environ.env.emit('gn_user_left', utils.activity_for_leave(user_id, user_name, room_id, room_name), room=room_id)
 
-        environ.env.db.remove_current_rooms_for_user(user_id)
+            environ.env.db.remove_current_rooms_for_user(user_id)
+        except Exception as e:
+            logger.error('could not leave all public rooms: %s' % str(e))
+            logger.debug('request for failed leave_all_public_rooms_and_emit_leave_events(): %s' % str(data))
+            logger.exception(traceback.format_exc())
 
     @staticmethod
     def set_user_offline(arg: tuple):
         data, activity = arg
         try:
-            user_id = str(environ.env.session.get(SessionKeys.user_id.value))
+            user_id = activity.actor.id
             environ.env.db.set_user_offline(user_id)
         except Exception as e:
             logger.error('could not set user offline: %s' % str(e))
@@ -59,10 +71,16 @@ class OnDisconnectHooks(object):
 
     @staticmethod
     def emit_disconnect_event(arg: tuple) -> None:
-        user_id = str(environ.env.session.get(SessionKeys.user_id.value))
-        user_name = environ.env.session.get(SessionKeys.user_name.value)
-        activity_json = utils.activity_for_disconnect(user_id, user_name)
-        environ.env.publish(activity_json, external=True)
+        data, activity = arg
+        try:
+            user_id = activity.actor.id
+            user_name = environ.env.session.get(SessionKeys.user_name.value)
+            activity_json = utils.activity_for_disconnect(user_id, user_name)
+            environ.env.publish(activity_json, external=True)
+        except Exception as e:
+            logger.error('could not emit disconnect event: %s' % str(e))
+            logger.debug('request for failed emit_disconnect_event(): %s' % str(data))
+            logger.exception(traceback.format_exc())
 
 
 @environ.env.observer.on('on_disconnect')
