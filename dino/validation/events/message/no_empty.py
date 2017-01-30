646091beed51243ed825ec61a5f2b6e173ea8c71
Oscar Eriksson <oscar.eriks@gmail.com>
2017-01-30 14:36:42 +0800

    plugin to forbid empty messages

---
+++ dino/validation/events/message/no_empty.py
@@ -0,0 +1,52 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import traceback
+import time
+
+from yapsy.IPlugin import IPlugin
+from activitystreams.models.activity import Activity
+
+from dino import utils
+from dino.environ import GNEnvironment
+
+logger = logging.getLogger()
+
+__author__ = 'Oscar Eriksson <oscar.eriks@gmail.com>'
+
+
+class OnMessageCheckBlackList(IPlugin):
+    def __init__(self):
+        super(OnMessageCheckBlackList, self).__init__()
+        self.env = None
+
+    def setup(self, env: GNEnvironment):
+        self.env = env
+
+    def get_black_list(self):
+        return self.env.db.get_black_list()
+
+    def _process(self, data: dict, activity: Activity):
+        message = activity.object.content
+        if message is None or len(message.strip()) == 0 or len(utils.b64d(message).strip()) == 0:
+            return False, 'message is empty'
+        return True, None
+
+    def __call__(self, *args, **kwargs) -> (bool, str):
+        data, activity = args[0], args[1]
+        try:
+            return self._process(data, activity)
+        except Exception as e:
+            logger.error('could not execute plugin no_empty: %s' % str(e))
+            logger.exception(traceback.format_exc())
+
