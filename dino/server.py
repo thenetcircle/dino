85ea4d48ccdc1e142b65f2abcb96b31e88b123b7
Oscar Eriksson <oscar.eriks@gmail.com>
2017-03-08 15:01:24 +0800

    no more engineio logging

--- dino/server.py
+++ dino/server.py
@@ -31,12 +31,11 @@ def create_app():
 
     # used for encrypting cookies for handling sessions
     _app.config['SECRET_KEY'] = 'secret!fdsa'
-    # os.environ.get('DINO_DEBUG', '0') == '1'
 
     _socketio = SocketIO(
             _app,
             logger=logger,
-            engineio_logger=logger,
+            engineio_logger=os.environ.get('DINO_DEBUG', '0') == '1',
             async_mode='eventlet',
             message_queue=environ.env.config.get(ConfigKeys.HOST, domain=ConfigKeys.QUEUE, default=''))
 
