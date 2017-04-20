2d5cbddb6df67014a2b7e296adaac081210eecea
Oscar Eriksson <oscar.eriks@gmail.com>
2017-04-20 18:36:17 +0800

    updated deploy script to handle tags

--- bin/deploy-simple.sh
+++ bin/deploy-simple.sh
@@ -28,11 +28,32 @@ if [ -z "$VIRTUAL_ENV" ]; then
     fi
 fi
 
+if [ -f .dino.yaml ]; then
+    echo "error: you need to copy your dino.yaml settings to '.dino.yaml' before upgrading dino"
+    exit 1
+fi
+
 SYSTEMD_PATH="/usr/lib/systemd/system"
 
-echo "pulling from git... "
-if ! git pull; then
-    echo "error: could not pull from git"
+echo "fetching new tags from git... "
+if ! git fetch; then
+    echo "error: could not fetch from git"
+    exit 1
+fi
+
+if ! git checkout -- dino.yaml; then
+    echo "error: could not clear changes to dino.yaml settings file"
+    exit 1
+fi
+
+echo "switching to tag $TAG_NAME... "
+if ! git checkout $TAG_NAME; then
+    echo "error: could not switch to specified dino tag $TAG_NAME"
+    exit 1
+fi
+
+if ! cp .dino.yaml dino.yaml; then
+    echo "error: after switching tag, could not copy .dino.yaml to dino.yaml"
     exit 1
 fi
 
