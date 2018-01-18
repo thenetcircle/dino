4cd9e6ba839531534287d1015b689f4377471d7d
Oscar Eriksson <oscar.eriks@gmail.com>
2018-01-18 12:23:40 +0800

    increase worker connections per node in start script

--- bin/dino.sh
+++ bin/dino.sh
@@ -78,7 +78,7 @@ DINO_ENVIRONMENT=$1 DINO_DEBUG=0 gunicorn \
     --workers 1 \
     --threads 1 \
     --keep-alive 5 \
-    --worker-connections 1000 \
+    --worker-connections 10000 \
     --statsd-host ${STATSD_HOST}:8125 \
     --statsd-prefix gunicorn-dino-${1}-${3}-${2} \
     --name dino-${1}-${3}-${2} \
