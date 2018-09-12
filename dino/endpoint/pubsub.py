aa9f02af7971805f3ca05eb6809adc32431357ac
Oscar Eriksson <oscar.eriks@gmail.com>
2018-09-12 14:12:33 +0800

    modulo operation causing weird behaviour

--- dino/endpoint/pubsub.py
+++ dino/endpoint/pubsub.py
@@ -242,7 +242,10 @@ class PubSub(object):
 
                     # try to get some consistency
                     try:
-                        partition = int(float(message.get('actor', dict()).get('id', 0))) % n_partitions
+                        actor = message.get('actor', dict())
+                        actor_id = actor.get('id', 0)
+                        actor_id = int(float(actor_id))
+                        partition = actor_id % n_partitions
                     except Exception as partition_e:
                         logger.exception(traceback.format_exc())
                         environ.env.capture_exception(partition_e)
