397c587514fc5a098cf1119893dedd48e4fc4266
Oscar Eriksson <oscar.eriks@gmail.com>
2018-11-08 12:27:13 +0800

    sleep to let rest node send restart event

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import eventlet
 # need to monkey patch some standard functions in python since they don't natively support async mode
 eventlet.monkey_patch()
 
+# let the rest node start first and send the restart event
+import time
+time.sleep(5)
+
 # keep this import; even though unused, gunicorn needs it, otherwise it will not start
 from dino.server import socketio, app
 
--- wio.py
+++ wio.py
@@ -3,6 +3,10 @@ import eventlet
 # need to monkey patch some standard functions in python since they don't natively support async mode
 eventlet.monkey_patch()
 
+# let the rest node start first and send the restart event
+import time
+time.sleep(5)
+
 import logging
 logging.getLogger('kafka').setLevel(logging.INFO)
 
