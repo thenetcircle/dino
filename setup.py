9835a9e6ea5540086f7158808f9936003259a8f4
Oscar Eriksson <oscar.eriks@gmail.com>
2019-01-30 10:51:42 +0800

    need to import the startup hooks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.16.49'
+version = '0.16.50'
 
 setup(
     name='dino',
--- web.py
+++ web.py
@@ -4,6 +4,7 @@ import eventlet
 eventlet.monkey_patch()
 
 # keep this import; even though unused, gunicorn needs it, otherwise it will not start
+from dino.hooks.startup import *
 from dino.web import app
 
 from dino import environ
