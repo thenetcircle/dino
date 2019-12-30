249b6e1a0d3bfa9e5271d7be81547fed158d1d75
Oscar Eriksson <oscar.eriks@gmail.com>
2019-12-30 11:20:31 +0800

    source bashrc

--- bin/count_users_in_rooms.sh
+++ bin/count_users_in_rooms.sh
@@ -4,5 +4,6 @@ CONDA_ENVIRONMENT=$1
 DINO_ENVIRONMENT=$2
 DINO_HOME=$3
 
+source ~/bin/.bashrc
 source activate ${CONDA_ENVIRONMENT}
 python /usr/local/bin/count_users_in_rooms.py ${DINO_ENVIRONMENT} ${DINO_HOME}
