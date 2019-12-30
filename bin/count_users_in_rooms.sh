55b86284c0d48c1bbb9dcce7656362fae500ba7e
Oscar Eriksson <oscar.eriks@gmail.com>
2019-12-30 11:25:48 +0800

    bashrc path for script

--- bin/count_users_in_rooms.sh
+++ bin/count_users_in_rooms.sh
@@ -3,7 +3,8 @@
 CONDA_ENVIRONMENT=$1
 DINO_ENVIRONMENT=$2
 DINO_HOME=$3
+BASHRC_PATH=$4
 
-source ~/bin/.bashrc
+source ${BASHRC_PATH}
 source activate ${CONDA_ENVIRONMENT}
 python /usr/local/bin/count_users_in_rooms.py ${DINO_ENVIRONMENT} ${DINO_HOME}
