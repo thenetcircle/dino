587ba51e7545113a4882c54f12aca1af99b8c198
Oscar Eriksson <oscar.eriks@gmail.com>
2020-09-03 14:14:14 +0800

    still leave if it's the current sid

--- dino/hooks/leave.py
+++ dino/hooks/leave.py
@@ -24,7 +24,8 @@ class OnLeaveHooks(object):
         # multi-login, can be in same room as another session
         sids = utils.sids_for_user_in_room(user_id, room_id)
         if sids is not None and len(sids) > 0:
-            return
+            if len(sids) > 1 or sids[0] != environ.env.request.sid:
+                return
 
         utils.remove_user_from_room(user_id, user_name, room_id)
 
