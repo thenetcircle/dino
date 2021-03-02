25376b38ff364588b059886d70348f9f1e22a030
Oscar Eriksson <oscar@thenetcircle.com>
2021-03-02 11:58:55 +0800

    json=True

--- dino/hooks/join.py
+++ dino/hooks/join.py
@@ -100,7 +100,7 @@ class OnJoinHooks(object):
             return
 
         activity_json = utils.activity_for_user_joined(user_id, user_name, room_id, room_name, image)
-        environ.env.out_of_scope_emit('gn_user_joined', activity_json, room=room_id, broadcast=True, namespace='/ws')
+        environ.env.out_of_scope_emit('gn_user_joined', activity_json, json=True, room=room_id, broadcast=True, namespace='/ws')
         environ.env.publish(activity_json, external=True)
 
 
