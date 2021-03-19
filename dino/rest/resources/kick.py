03c052ea0958a605459fe0ee8f2eae01e9b2d566
Oscar Eriksson <oscar@thenetcircle.com>
2021-03-19 15:43:47 +0800

    need to decode room name

--- dino/rest/resources/kick.py
+++ dino/rest/resources/kick.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python
-
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 import logging
 import traceback
@@ -20,6 +7,7 @@ from dino.utils.decorators import timeit
 from dino.db.manager import UserManager
 from dino.rest.resources.base import BaseResource
 
+from dino import utils
 from flask import request
 
 logger = logging.getLogger(__name__)
@@ -61,6 +49,7 @@ class KickResource(BaseResource):
                     room_id = kick_info.get('room_id')
                 else:
                     room_name = kick_info.get('room_name')
+                    room_name = utils.b64d(room_name)
 
                 if room_id is not None and not len(room_id.strip()):
                     room_id = None
