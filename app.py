b7431abd4901ddc875ca0d192313a83f596281b5
Oscar Eriksson <oscar.eriks@gmail.com>
2022-02-08 15:07:16 +0800

    dep version fixes

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ time.sleep(int(5+random.random()*10))
 
 import logging
 logging.getLogger('kafka').setLevel(logging.INFO)
+logging.getLogger('yapsy').setLevel(logging.INFO)
 
 # keep this import; even though unused, gunicorn needs it, otherwise it will not start
 from dino.server import socketio, app
