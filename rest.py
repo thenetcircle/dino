dfceec1e2b86039e7fe8687355cde478ce785fee
Oscar Eriksson <oscar@thenetcircle.com>
2022-07-11 18:04:01 +0800

    limit yapsy output in rest/web nodes too

--- rest.py
+++ rest.py
@@ -8,6 +8,7 @@ logging.getLogger('kafka').setLevel(logging.INFO)
 logging.getLogger('kafka.protocol.parser').setLevel(logging.INFO)
 logging.getLogger('kafka.producer.sender').setLevel(logging.INFO)
 logging.getLogger('kafka.producer.record_accumulator').setLevel(logging.INFO)
+logging.getLogger('yapsy').setLevel(logging.INFO)
 
 # keep this import; even though unused, gunicorn needs it, otherwise it will not start
 from dino.hooks import *
