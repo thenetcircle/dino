8b18623160f5a039f0ed52a371e4db007e6938ba
Oscar Eriksson <oscar@thenetcircle.com>
2022-07-11 17:42:57 +0800

    need blinker for flask sentry integration

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.21.1'
+version = '0.21.2-dev'
 
 setup(
     name='dino',
@@ -51,5 +51,6 @@ setup(
         'pymitter',
         'psutil',
         'gitpython',
-        'sentry-sdk'
+        'sentry-sdk',
+        'blinker'
     ])
