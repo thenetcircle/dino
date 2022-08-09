a22f0afa320a1b2494a0aa61ee0d5d134a1f235d
Oscar Eriksson <oscar@thenetcircle.com>
2022-08-09 10:23:11 +0800

    change to get method, let flask serialize to json

--- dino/rest/resources/dump_cache.py
+++ dino/rest/resources/dump_cache.py
@@ -1,7 +1,9 @@
 import logging
+from functools import lru_cache
 
 from flask import request
 
+from datetime import datetime
 from dino import environ
 from dino.rest.resources.base import BaseResource
 from dino.utils.decorators import timeit
@@ -12,11 +14,25 @@ logger = logging.getLogger(__name__)
 class DumpCacheResource(BaseResource):
     def __init__(self):
         super(DumpCacheResource, self).__init__()
+        self.last_cleared = datetime.utcnow()
         self.request = request
         self.namespace = "/ws"
 
-    @timeit(logger, "on_rest_dump_cache_cache")
-    def do_post(self):
+    def _get_lru_method(self):
+        return self.get_cache
+
+    def _get_last_cleared(self):
+        return self.last_cleared
+
+    def _set_last_cleared(self, last_cleared):
+        self.last_cleared = last_cleared
+
+    @lru_cache()
+    def get_cache(self):
         # mock cache doesn't have in-memory cache
         if hasattr(environ.env.cache, "cache"):
             return environ.env.cache.cache.vals
+
+    @timeit(logger, "on_rest_dump_cache_cache")
+    def do_get(self):
+        return self.get_cache()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 
-version = '0.21.16-dev'
+version = '0.21.16'
 
 setup(
     name='dino',
