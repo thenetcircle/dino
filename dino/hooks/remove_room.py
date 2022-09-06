20423196aa0d9a93ef6033b403fcaa6ecedbd7bd
Oscar Eriksson <oscar.eriks@gmail.com>
2022-09-06 10:04:27 +0800

    it's a dict already

--- dino/hooks/remove_room.py
+++ dino/hooks/remove_room.py
@@ -11,33 +11,11 @@ class OnRemoveRoomHooks(object):
     def publish_event(arg: tuple) -> None:
         _, activity = arg
 
-        json_act = {
-            'id': activity.id,
-            'published': activity.published,
-            'actor': {
-                'id': activity.actor.id,
-                'displayName': activity.actor.display_name
-            },
-            'target': {
-                'id': activity.target.id,
-                'displayName': activity.target.display_name,
-                'objectType': activity.target.object_type
-            },
-            'object': {
-                'content': activity.object.content,
-                'objectType': activity.object.object_type
-            },
-            'verb': activity.verb,
-            'provider': {
-                'id': activity.provider.id
-            }
-        }
-
         if len(environ.env.config) == 0 or environ.env.config.get(ConfigKeys.TESTING, False):
             # assume we're testing
             return
 
-        environ.env.publish(json_act, external=True)
+        environ.env.publish(activity, external=True)
 
 
 @environ.env.observer.on('on_remove_room')
--- dino/hooks/rename_room.py
+++ dino/hooks/rename_room.py
@@ -11,30 +11,11 @@ class OnRenameRoomHooks(object):
     def publish_event(arg: tuple) -> None:
         _, activity = arg
 
-        json_act = {
-            'id': activity.id,
-            'published': activity.published,
-            'actor': {
-                'id': activity.actor.id,
-                'displayName': activity.actor.display_name
-            },
-            'target': {
-                'id': activity.target.id,
-                'displayName': activity.target.display_name,
-                'objectType': activity.target.object_type,
-                'summary': activity.target.summary
-            },
-            'verb': activity.verb,
-            'provider': {
-                'id': activity.provider.id
-            }
-        }
-
         if len(environ.env.config) == 0 or environ.env.config.get(ConfigKeys.TESTING, False):
             # assume we're testing
             return
 
-        environ.env.publish(json_act, external=True)
+        environ.env.publish(activity, external=True)
 
 
 @environ.env.observer.on('on_rename_room')
