a1ff148bab3b1e6115ab9ecfb5358f39bada4f50
Oscar Eriksson <oscar@thenetcircle.com>
2023-08-16 11:47:15 +0800

    debug logging

--- dino/validation/acl.py
+++ dino/validation/acl.py
@@ -72,6 +72,9 @@ class AclValidator(object):
             session_to_use = environ.env.session
 
         all_acls = environ.env.config.get(ConfigKeys.ACL)
+        logger.info('all_acls:')
+        logger.info(all_acls)
+        logger.info("")
 
         if not hasattr(activity, 'target') or not hasattr(activity.target, 'object_type'):
             return False, 'target.objectType must not be none'
@@ -84,8 +87,8 @@ class AclValidator(object):
             object_type = activity.target.object_type
 
         # one-to-one is sending message that users private room, so target is room, but object_type would not be
-        if target == ApiTargets.ROOM and object_type != 'room':
-            return True, None
+        # if target == ApiTargets.ROOM and object_type != 'room':
+        #     return True, None
 
         user_id = activity.actor.id
         if target == 'room':
@@ -95,31 +98,40 @@ class AclValidator(object):
                 channel_id = activity.object.url
 
         if utils.is_admin(channel_id, user_id):
+            logger.info(f'user {user_id} is admin')
             return True, None
         if utils.is_super_user(user_id):
+            logger.info(f'user {user_id} is super user')
             return True, None
         if utils.is_global_moderator(user_id):
+            logger.info(f'user {user_id} is global moderator')
             return True, None
 
         # no acls for this target (room/channel) and action (join/kick/etc)
         if target not in all_acls or action not in all_acls[target] or len(all_acls[target][action]) == 0:
+            logger.info('no acls for target "%s" and action "%s"' % (target, action))
+            logger.info(f'action: {action}, target: {target}, alL_acls[target]: {all_acls[target]}')
             return True, None  # 'no acl set that allows action "%s" for target type "%s"' % (action, target)
 
         if utils.is_owner_channel(channel_id, user_id):
+            logger.info(f'user {user_id} is owner of channel: {channel_id}')
             return True, None
 
         if target == 'channel':
             pass
         elif target == 'room':
             if utils.is_owner(target_id, user_id):
+                logger.info(f'user {user_id} is owner of room: {target_id}')
                 return True, None
 
         # no acls for this target and action
         if target_acls is None or len(target_acls) == 0:
+            logger.info('no target_acls')
             return True, None
 
         possible_acls = all_acls[target][action]
         for acl_rule, acl_values in possible_acls.items():
+            logger.info(f'checking acl rule: {acl_rule}, acl_values: {acl_values}')
             if acl_rule != 'acls':
                 continue
             for acl in acl_values:
@@ -132,6 +144,7 @@ class AclValidator(object):
                     return False, 'acl "%s" did not validate for target acl "%s": %s' % (
                         acl, target_acls[acl], msg)
 
+        logger.info('all acls validated')
         return True, None
 
 
