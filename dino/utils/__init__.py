da9b6ea27ad0b62b92e8234c56f2206067600609
Oscar Eriksson <oscar.eriks@gmail.com>
2023-11-28 12:50:43 +0800

    get username from redis if wio node

--- dino/utils/__init__.py
+++ dino/utils/__init__.py
@@ -1048,6 +1048,11 @@ def activity_for_users_in_room(activity: Activity, users_orig: dict) -> dict:
 
     for user_id, user_name in users.items():
         user_info = get_user_info_attachments_for(user_id)
+
+        # for WIO we don't have the username in the db (so name equals id), so get it from redis instead
+        if (user_name == user_id or not len(user_name)) and len(user_info[SessionKeys.user_name.value]):
+            user_name = b64d(user_info[SessionKeys.user_name.value])
+
         if this_user_is_super_user:
             user_ip = ''
             try:
